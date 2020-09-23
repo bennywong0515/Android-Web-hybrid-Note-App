e9f953b8fcddf29ad484dcf2b245759dfffe0f81
bennywong0515 <71306414+bennywong0515@users.noreply.github.com>
2020-09-23 13:12:29 +0800

    upload

---
+++ HTMLWebApp/app/src/main/java/com/benny/htmlwebapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.benny.htmlwebapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        myWebView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return true;
+            }
+        });
+        myWebView.setLongClickable(false);
+        myWebView.loadUrl("file:///android_asset/index.html");
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.addJavascriptInterface(new JavaScriptInterface(this), "JSInterface");
+    }
+}
